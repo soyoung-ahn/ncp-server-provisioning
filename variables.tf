ad0267d82801b11c2133c3657c359d834427b2a7
jiseooooon <57252187+jiseooooon@users.noreply.github.com>
2022-11-29 15:22:33 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "KR"
+}
+
+variable "subnet_no" {
+  default = "66177"
+}
+
+variable "server_image_product_code" {
+  default = "" // 추가 필요 
+}
+
+variable "server_login_key_name" {
+  default = "ncp-demo-ssh-key"
+}

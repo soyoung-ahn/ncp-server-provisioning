a965d40ca6a279f07ab1b65221989f3d83ea9b51
soyoung-ahn <89114528+soyoung-ahn@users.noreply.github.com>
2022-11-30 18:01:34 +0900

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,6 +1,6 @@
 resource "ncloud_server" "server" {
   subnet_no                 = var.subnet_no
-  name                      = var.server_name
+  server_name               = var.server_name
   server_image_product_code = var.server_image_product_code
   login_key_name            = var.server_login_key_name
 }
